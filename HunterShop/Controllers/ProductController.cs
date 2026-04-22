4b0fdc7155a5d05e364a525d9fcb51a9abcc72c6
gutnovaalina <gutnovaalina@gmail.com>
2026-04-22 13:41:13 +0300

    Добавьте файлы проекта.

---
+++ HunterShop/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using HunterShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HunterShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult AllProducts()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product("лук тисовый","лук выполнен из ценной породы дерева",5000),
+                new Product("ружье дальнобойное","дистанция 500 м",12000)
+            };
+            return View(products);
+        }
+    }
+}
